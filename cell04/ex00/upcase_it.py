0170fae063be8b72f445424bb96146c8a710402f
kwongdee <67010106@kmitl.ac.th>
2025-05-06 10:02:56 +0700

    Add files via upload

---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,3 @@
+txt = input("Give me a word: ")
+txt = txt.upper()
+print(txt)
---
+++ cell04/ex01/ age.py
@@ -0,0 +1,7 @@
+age = int(input("Please tell me your age: "))
+print(f"You are currently {age} years old.")
+
+i=10
+for i in range(1,4):
+    in_year = i *10
+    print(f"In {in_year} years, you'll be {age+in_year} years old.")
